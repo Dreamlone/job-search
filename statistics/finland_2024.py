33e87e45657386db81f8440999bc5e0e23a61f23
Dreamlone <mik_sar@mail.ru>
2024-01-28 19:33:54 +0200

    df interview added

--- statistics/finland_2024.py
+++ statistics/finland_2024.py
@@ -8,7 +8,6 @@ warnings.filterwarnings('ignore')
 
 
 def calculate_statistics():
-    """ Train model and save it """
     df = pd.read_csv(Path('../data/finland_2024.csv'))
     for col in ['Date of application', 'Contact back',
                 'First interview', 'Second interview', 'Third interview']:
@@ -21,9 +20,9 @@ def calculate_statistics():
     df_rejected = df[df['Status'] == 'rejected']
     print(f'Number of rejected: {len(df_rejected)}, per cent: {(len(df_rejected) / all_items_number) * 100:.0f}')
 
-    got_invitations_number = all_items_number - len(df_not_answered) - len(df_rejected)
-    # df_interviewed = df[df['First interview'] != pd.Nan]
-    print(f'Number of invitations to the first interview round: {got_invitations_number}, per cent: {(got_invitations_number / all_items_number) * 100:.0f}')
+    df_interviewed = df.query(f'Status != ["not answered", "rejected"]')
+    print(f'Number of invitations to the first interview round: {len(df_interviewed)}, '
+          f'per cent: {(len(df_interviewed) / all_items_number) * 100:.0f}')
 
     df_rejected['Waiting time'] = df_rejected['Contact back'] - df_rejected['Date of application']
     print(f'Average waiting time to get rejection: {df_rejected["Waiting time"].mean().days}')
